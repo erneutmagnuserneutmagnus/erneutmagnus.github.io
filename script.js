27200f347a9d14e4245bc4954dc24b1f5b411f9f
erneutmagnuserneutmagnus <erneutmagnuserneutmagnus@gmail.com>
2025-03-26 09:43:10 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function changeColor() {
-    const neonButton = document.querySelector('.neon-button');
-    neonButton.style.backgroundColor = neonButton.style.backgroundColor === 'transparent' ? '#0ff' : 'transparent';
-    neonButton.style.color = neonButton.style.color === '#0ff' ? '#111' : '#0ff';
-}
